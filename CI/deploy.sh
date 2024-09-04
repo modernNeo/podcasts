3e6889e2e85b602a369b3de50cb31edeafe19740
Jace Manshadi <jaymanshad@proton.me>
2024-09-04 07:35:02 -0700

    making sure the puller container is also updated

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -8,11 +8,13 @@ set -e -o xtrace
 export COMPOSE_PROJECT_NAME="podcasts_site"
 
 export prod_container_name="${COMPOSE_PROJECT_NAME}_app"
+export prod_container_puller_name="${COMPOSE_PROJECT_NAME}_puller"
 export prod_container_db_name="${COMPOSE_PROJECT_NAME}_db"
 export docker_compose_file="CI/docker-compose.yml"
 export prod_image_name_lower_case=$(echo "$prod_container_name" | awk '{print tolower($0)}')
 
 docker rm -f ${prod_container_name} || true
+docker rm -f ${prod_container_puller_name} || true
 docker image rm -f $(docker images  | grep -i "${prod_image_name_lower_case}" | awk '{print $3}') || true
 docker volume create --name="${COMPOSE_PROJECT_NAME}_logs"
 docker compose -f "${docker_compose_file}" up -d
