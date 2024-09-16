0af36e97ae6b1857db6b137df13ca0d434ad4dc9
Jace Manshadi <jaymanshad@proton.me>
2024-09-15 21:23:56 -0700

    fixing date format for national

--- podcasts_site/podcasts_site/settings.py
+++ podcasts_site/podcasts_site/settings.py
@@ -143,9 +143,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CBC_NEWS_TITLE_DATE_NORMALIZER = {
-"Sept.": "Sep"
+    "Sept.": "Sep"
 }
-THE_NATIONAL_DATE_FORMAT = "%b %d, %Y"
+THE_NATIONAL_DATE_FORMAT = "%B %d, %Y"
 CBC_NEWS_DATE_FORMAT = ["%I, %b %d", "%I%M, %b %d", "%I, %B %d"]
 CBC_VANCOUVER_NEWS_PREFIX = 'CBC Vancouver News at '
 THE_NATIONAL_CHAPTER_PREFIX = 'The National for '
