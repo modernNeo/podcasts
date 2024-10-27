937589dce265ac2521a36f1fe21575ad277b896b
Jace Manshadi <jaymanshad@proton.me>
2024-10-27 11:48:18 -0700

    making sure description gets updated only when there is a description in the video as a blank description causes isues with the rss feed generator

--- podcasts_site/podcasts/views/update_podcast_being_processed.py
+++ podcasts_site/podcasts/views/update_podcast_being_processed.py
@@ -5,7 +5,8 @@ from podcasts.views.pstdatetimefield import pstdatetime
 def update_podcast_being_processed(podcast_being_processed : YouTubePodcast, timestamp: pstdatetime, information, thumbnail):
     if podcast_being_processed.information_last_updated is None or timestamp > podcast_being_processed.information_last_updated:
         podcast_being_processed.name = information['playlist']
-        podcast_being_processed.description = information['description']
+        if len(information['description']) > 0:
+            podcast_being_processed.description = information['description']
         podcast_being_processed.image = thumbnail
         podcast_being_processed.language = "en-US"
         podcast_being_processed.author = information['uploader']
