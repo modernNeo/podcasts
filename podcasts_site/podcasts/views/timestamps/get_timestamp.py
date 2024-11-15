9d5e0c8995c52b832d6ea8bded5c90f56389deae
Jace Manshadi <jaymanshad@proton.me>
2024-11-14 20:56:10 -0800

    updating code so that the year, month and day for a cbc vancouver news video is pulled from the timestamp in the information dict since sometimes they use the wrong date on the title itself

--- podcasts_site/podcasts/views/timestamps/get_timestamp.py
+++ podcasts_site/podcasts/views/timestamps/get_timestamp.py
@@ -12,7 +12,15 @@ def get_timestamp(information, current_file_name, podcast_being_processed):
     cbc_vancouver_news_video = is_cbc_vancouver_video(current_file_name)
     the_national_video = video_has_national_in_first_chapter(information)
     if cbc_vancouver_news_video:
-        timestamp = get_cbc_vancouver_timestamp(current_file_name).pst
+        file_name_timestamp = get_cbc_vancouver_timestamp(current_file_name).pst
+        video_timestamp = pstdatetime.from_epoch(int(information['timestamp'])).pst
+        if video_timestamp.hour < 3:
+            video_timestamp = video_timestamp - timedelta(hours=6)
+        timestamp = pstdatetime(
+            year=video_timestamp.year, month=video_timestamp.month, day=video_timestamp.day,
+            hour=file_name_timestamp.hour, minute=file_name_timestamp.minute,
+            second=0, tzinfo=pstdatetime.PACIFIC_TZ
+        )
     elif the_national_video or podcast_being_processed.name == 'The National | Full Show':
         manually_extract = not the_national_video
         if the_national_video:
