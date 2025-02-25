08e49eb0d9072a0644603bec1733abd204e8d6b0
Jace Manshadi <jaymanshad@proton.me>
2025-02-25 05:04:15 -0800

    adding more logging to figure out why bill burr podcast is not being downloaded

--- podcasts_site/podcasts/views/match_filter.py
+++ podcasts_site/podcasts/views/match_filter.py
@@ -2,6 +2,7 @@ from podcasts.models import YouTubePodcast
 
 
 def match_filter(info, *, incomplete):
+    youtube_dlp_logger = Loggers.get_logger("youtube_dlp")
     title = info.get("title")
     if title is None:
         # it's processing the podcast info
@@ -10,6 +11,7 @@ def match_filter(info, *, incomplete):
         # processing a video that is not yet uploaded
         return f"{title} is not yet uploaded"
     youtube_podcast = YouTubePodcast.objects.all().filter(being_processed=True).first()
+    youtube_dlp_logger.info(f"match_filter processing podcast {youtube_podcast}")
     if youtube_podcast:
         substrings = list(
             youtube_podcast.youtubepodcasttitlesubstring_set.all()
@@ -19,6 +21,7 @@ def match_filter(info, *, incomplete):
             youtube_podcast.youtubepodcasttitleprefix_set.all()
             .values_list('title_prefix', flat=True)
         )
+        youtube_dlp_logger.info(f"found {len(substrings)} possible substrings: {substrings}")
         if len(substrings) > 0 or len(prefixes) > 0:
             matches_substring_filter = [
                 1
@@ -29,6 +32,7 @@ def match_filter(info, *, incomplete):
                 1 for prefix in prefixes
                 if title[:len(prefix)] == prefix
             ]
+            youtube_dlp_logger.info(f"{matches_substring_filter} substrings matched")
             if len(matches_substring_filter) > 0:
                 pass  # keep it
             if len(matches_prefix_filter) > 0:
