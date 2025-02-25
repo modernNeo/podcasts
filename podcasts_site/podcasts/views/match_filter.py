42ae05ee55da7e8e9de82cdbd39a5fd9f83f2295
Jace Manshadi <jaymanshad@proton.me>
2025-02-25 05:13:52 -0800

    fixing bug in how substring filters work

--- podcasts_site/podcasts/views/match_filter.py
+++ podcasts_site/podcasts/views/match_filter.py
@@ -3,7 +3,7 @@ from podcasts.views.setup_logger import Loggers
 
 
 def match_filter(info, *, incomplete):
-    youtube_dlp_logger = Loggers.get_logger("youtube_dlp")
+    # youtube_dlp_logger = Loggers.get_logger("youtube_dlp")
     title = info.get("title")
     if title is None:
         # it's processing the podcast info
@@ -12,7 +12,7 @@ def match_filter(info, *, incomplete):
         # processing a video that is not yet uploaded
         return f"{title} is not yet uploaded"
     youtube_podcast = YouTubePodcast.objects.all().filter(being_processed=True).first()
-    youtube_dlp_logger.info(f"match_filter processing podcast {youtube_podcast}")
+    # youtube_dlp_logger.info(f"match_filter processing podcast {youtube_podcast}")
     if youtube_podcast:
         substrings = list(
             youtube_podcast.youtubepodcasttitlesubstring_set.all()
@@ -22,7 +22,7 @@ def match_filter(info, *, incomplete):
             youtube_podcast.youtubepodcasttitleprefix_set.all()
             .values_list('title_prefix', flat=True)
         )
-        youtube_dlp_logger.info(f"found {len(substrings)} possible substrings: {substrings}")
+        # youtube_dlp_logger.info(f"found {len(substrings)} possible substrings: {substrings}")
         if len(substrings) > 0 or len(prefixes) > 0:
             matches_substring_filter = [
                 1
@@ -33,10 +33,10 @@ def match_filter(info, *, incomplete):
                 1 for prefix in prefixes
                 if title[:len(prefix)] == prefix
             ]
-            youtube_dlp_logger.info(f"{matches_substring_filter} substrings matched")
+            # youtube_dlp_logger.info(f"{matches_substring_filter} substrings matched")
             if len(matches_substring_filter) > 0:
                 pass  # keep it
-            if len(matches_prefix_filter) > 0:
+            elif len(matches_prefix_filter) > 0:
                 pass  # keep it
             else:
                 return f"{title} is not needed"
