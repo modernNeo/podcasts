26d84270309580e7b14ca742d92a516ae04edb0e
Jace Manshadi <jaymanshad@proton.me>
2025-04-09 14:59:58 -0700

    uncommenting original log command to see if increases runtime

--- podcasts_site/podcasts/views/match_filter.py
+++ podcasts_site/podcasts/views/match_filter.py
@@ -12,8 +12,7 @@ def match_filter(info, incomplete):
         return
     youtube_dlp_logger = Loggers.get_logger("youtube_dlp")
     # info['automatic_captions'] = None
-    # youtube_dlp_logger.info(f"{title}'s info is {json.dumps(info, indent=4)}")
-    youtube_dlp_logger.info(f"{title}'s info is {info}")
+    youtube_dlp_logger.info(f"{title}'s info is {json.dumps(info, indent=4)}")
     if info.get('live_status', None) == 'is_upcoming' or info.get('live_status', None) == 'is_live':
         # processing a video that is not yet uploaded
         return f"{title} is not yet uploaded"
