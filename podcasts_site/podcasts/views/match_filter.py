dd34d71d857a1c15cedc1ce1a8b2768dbbeca2c1
Jace Manshadi <jaymanshad@proton.me>
2025-09-25 15:54:32 -0700

    need to figure out why size is not being detected

--- podcasts_site/podcasts/views/match_filter.py
+++ podcasts_site/podcasts/views/match_filter.py
@@ -12,7 +12,7 @@ def match_filter(info, incomplete):
     youtube_dlp_logger = Loggers.get_logger("youtube_dlp")
     # info['automatic_captions'] = None
     # info['formats'] = None
-    # youtube_dlp_logger.info(f"{title}'s info is {json.dumps(info, indent=4)}")
+    youtube_dlp_logger.info(f"{title}'s info is {json.dumps(info, indent=4)}")
     youtube_dlp_logger.info(f"matching [{title}]")
     if info.get('live_status', None) == 'is_upcoming' or info.get('live_status', None) == 'is_live':
         # processing a video that is not yet uploaded
