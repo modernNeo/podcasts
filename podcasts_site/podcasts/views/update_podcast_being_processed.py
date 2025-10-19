1ca39f27c9b45a01d702d3b00708c63b2e5c10cd
Jace Manshadi <jaymanshad@proton.me>
2025-10-19 16:57:58 -0700

    apparently sometimes uploader is None

--- podcasts_site/podcasts/views/update_podcast_being_processed.py
+++ podcasts_site/podcasts/views/update_podcast_being_processed.py
@@ -9,7 +9,8 @@ def update_podcast_being_processed(podcast_being_processed : YouTubePodcast, tim
             podcast_being_processed.description = information['description']
         podcast_being_processed.image = thumbnail
         podcast_being_processed.language = "en-US"
-        podcast_being_processed.author = information['uploader']
+        podcast_being_processed.author = information['uploader'] \
+            if information['uploader'] is not None else information['playlist_uploader']
         podcast_being_processed.category = information['categories'][0]
         podcast_being_processed.information_last_updated = timestamp
         podcast_being_processed.save()
