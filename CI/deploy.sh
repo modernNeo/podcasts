58530fd90fe544e5c71a4ed1d68d413a18e3c102
Jace Manshadi <jaymanshad@proton.me>
2026-08-31 13:35:18 -0400

    log the current output as checking progress

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -26,13 +26,14 @@ sleep 20
 container_db_failed=$(docker ps -a -f name=${prod_container_db_name} --format "{{.Status}}" | head -1)
 
 if [[ "${container_db_failed}" != *"Up"* ]]; then
-    docker logs ${prod_container_db_name}
+    docker logs  --tail 50 ${prod_container_db_name}
     exit 1
 fi
 
 
 # 1. Loop until the container is no longer running
 while [ "$(docker inspect -f '{{.State.Running}}' "$prod_container_name" 2>/dev/null)" = "true" ]; do
+    docker logs  --tail 50 $prod_container_name
     sleep 2
 done
 
@@ -49,6 +50,6 @@ elif [ "$EXIT_CODE" -eq 0 ]; then
     exit 0
 else
     echo "Container failed with exit code: $EXIT_CODE"
-    docker logs $prod_container_name
+    docker logs  --tail 50 $prod_container_name
     exit "$EXIT_CODE"
 fi
